53a74a17ffdc30ddb89a2deba276530e6b04f74f
Turbien <clemeblav@gmail.com>
2024-02-05 13:58:22 +0100

    feat: loadMap() start
    - loadMap loads the file
    - started the code for parsing the .txt file

--- src/main/java/model/level/MapModel.java
+++ src/main/java/model/level/MapModel.java
@@ -1,6 +1,13 @@
 package model.level;
 
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class MapModel {
     private static TileModel[][] tiles;
 
@@ -48,8 +55,30 @@ public class MapModel {
      *      <li> 'b', '/', 'r' and 's' are just for the show, no actual meaning for now </li>
      * </ul>
      */
-    public static void loadMap(String path){
 
+
+    public static String[][] parseMap(List<String> lines){
+        int height = lines.size();
+        int width = 2 * ((lines.get(0).length()-1)/4) + 1;
+        String[][] arr = new String[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+
+            }
+        }
+        return arr;
     }
+    public static void loadMap(String path) throws IOException {
+        try {
+            InputStream file = Objects.requireNonNull(MapModel.class.getResourceAsStream(path));
+            byte[] buffer = file.readAllBytes();
+            String stringData = new String(buffer, StandardCharsets.UTF_8);
+            List<String> list = Arrays.asList(stringData.split("\n"));
+            parseMap(list);
+        }
+        catch ( NullPointerException e) { e.printStackTrace(); }
+        
+    }
+
 
 }
