a6dcaa43c79da757759c42064e423d6765f629a0
ZITOUNI Yacob <yacob.zitouni@etu.u-paris.fr>
2024-02-16 09:28:37 +0100

    feat: basic game loop with swing's timer (#6)

--- src/main/java/model/ingame/IUpdateable.java
+++ src/main/java/model/ingame/IUpdateable.java
@@ -1,9 +1,9 @@
 package model.ingame;
 
+/**
+ * Represents a function meant to be called at every tick of the game loop.
+ * An updater can itself call other update methods, and so on, to update the whole game state.
+ */
 public interface IUpdateable {
-    
-
     void update();
-
-    
 }
