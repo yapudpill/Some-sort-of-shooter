2f771569bb5ddcb566b5d734f020b4e8156b1c4c
Mohamed BEN EL MOSTAPHA <benelmed36@gmail.com>
2024-04-13 16:17:23 +0200

    refactor: use canEnterConditions set
    This commit changes the way the canEnter method is implemented in the
    TileModel class. Instead of using a method, a set of predicates is used
    in the canEnter function. This allows for more flexibility in the
    conditions that can be checked.

--- src/main/java/model/level/TileModel.java
+++ src/main/java/model/level/TileModel.java
@@ -2,17 +2,20 @@ package model.level;
 
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.function.Predicate;
 
 import model.ingame.entity.ICollisionEntity;
 import model.ingame.entity.IEntity;
 
 public abstract class TileModel implements ITileModel {
     protected final Set<ICollisionEntity> collidables = new CopyOnWriteArraySet<>();
+    protected Set<Predicate<IEntity>> canEnterConditions = new CopyOnWriteArraySet<>();
 
     public abstract boolean isWalkable();
 
     public boolean canEnter(IEntity entity) {
-        return true;
+        if(canEnterConditions.isEmpty()) return true;
+        return canEnterConditions.stream().allMatch(condition -> condition.test(entity));
     }
 
     @Override
@@ -39,5 +42,13 @@ public abstract class TileModel implements ITileModel {
 
     public void printCollidables() {
         System.out.println(collidables);
+    }
+
+    public void addCanEnterCondition(Predicate<IEntity> condition) {
+        canEnterConditions.add(condition);
+    }
+
+    public void removeCanEnterCondition(Predicate<IEntity> condition) {
+        canEnterConditions.remove(condition);
     }
 }
