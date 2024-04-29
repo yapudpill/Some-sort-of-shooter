982d075c6d5dbb33fb61b6901abfbdbec389a54c
ZITOUNI Yacob <yacob.zitouni@etu.u-paris.fr>
2024-04-29 18:52:50 +0200

    feat(gui.ingame): added tile animations

---
+++ src/main/java/gui/ingame/tile/StaticTileImageRenderer.java
@@ -0,0 +1,19 @@
+package gui.ingame.tile;
+
+import gui.ImageCache;
+
+import java.awt.*;
+
+public class StaticTileImageRenderer extends TileRenderer {
+    private final Image image;
+
+    public StaticTileImageRenderer(String path) {
+        image = ImageCache.loadImage(path, getClass());
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+    }
+}
