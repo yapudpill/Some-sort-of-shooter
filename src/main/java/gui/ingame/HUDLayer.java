d4484c2d322684c09ebc07df4ffa1388eb73a114
Yacob Zitouni <yacob.zitouni@gmail.com>
2024-05-18 15:13:50 +0200

    fix(gui.ingame): fix HUD to work even without a weapon + set default weapon to knife

--- src/main/java/gui/ingame/HUDLayer.java
+++ src/main/java/gui/ingame/HUDLayer.java
@@ -1,12 +1,5 @@
 package gui.ingame;
 
-import java.awt.Color;
-import java.util.function.IntSupplier;
-
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 import gui.IScalableComponent;
 import gui.ImageCache;
 import gui.ScaleLayout;
@@ -15,6 +8,10 @@ import model.ingame.entity.PlayerModel;
 import model.ingame.entity.WeaponEntity;
 import util.Coordinates;
 import util.IUpdateable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.function.IntSupplier;
 
 public class HUDLayer extends JPanel implements IUpdateable {
     private String currentMessString = "";
@@ -62,12 +59,15 @@ public class HUDLayer extends JPanel implements IUpdateable {
 
     @Override
     public void update(double delta) {
-        weaponLabel.setIcon(new ImageIcon(ImageCache.loadImage(String.format("sprites/weapon/%s.png", playerModel.getWeapon().getIdentifier()),PlayerRenderer.class)));
-        if(playerModel.isCurrentlyCollidingWith(e -> e instanceof WeaponEntity)) {
-            currentMessString = "Press E to pick up weapon";
-        } else {
-            currentMessString = "";
+        if (playerModel.getWeapon() != null) {
+            weaponLabel.setIcon(new ImageIcon(ImageCache.loadImage(String.format("sprites/weapon/%s.png", playerModel.getWeapon().getIdentifier()), PlayerRenderer.class)));
+            if (playerModel.isCurrentlyCollidingWith(e -> e instanceof WeaponEntity)) {
+                currentMessString = "Press E to pick up weapon";
+            } else {
+                currentMessString = "";
+            }
+            messageLabel.setText(currentMessString);
         }
-        messageLabel.setText(currentMessString);
+        else weaponLabel.setIcon(null);
     }
 }
