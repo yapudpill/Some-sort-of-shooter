daf1b58aa89f42053a93c5fb4dfb23931a2eac2c
Yacob Zitouni <yacob.zitouni@gmail.com>
2024-05-19 23:42:13 +0200

    fix: added new Weapons to ScenarioElementsParsers

--- src/main/java/model/level/scenario/ScenarioElementsParsers.java
+++ src/main/java/model/level/scenario/ScenarioElementsParsers.java
@@ -1,12 +1,21 @@
 package model.level.scenario;
 
-import model.ingame.entity.*;
+import model.ingame.entity.EntityConstructor;
+import model.ingame.entity.ExplodingEnemy;
+import model.ingame.entity.FirstAidKit;
+import model.ingame.entity.LootEnnemy;
+import model.ingame.entity.SmartEnemyModel;
+import model.ingame.entity.WalkingEnemyModel;
+import model.ingame.weapon.FlameThrower;
+import model.ingame.weapon.Gatling;
+import model.ingame.weapon.GravityGun;
 import model.ingame.weapon.KnifeWeapon;
 import model.ingame.weapon.PistolModel;
 import model.ingame.weapon.RocketLauncher;
 import model.ingame.weapon.RubberWeapon;
 import model.ingame.weapon.ShotGun;
 import model.ingame.weapon.SimpleTrapPlacer;
+import model.ingame.weapon.TornadoGun;
 import model.ingame.weapon.WeaponConstructor;
 
 /**
@@ -23,6 +32,10 @@ public class ScenarioElementsParsers {
             case "ShotGun" -> ShotGun::new;
             case "RubberWeapon" -> RubberWeapon::new;
             case "TrapPlacer" -> SimpleTrapPlacer::new;
+            case "TornadoGun" -> TornadoGun::new;
+            case "FlameThrower" -> FlameThrower::new;
+            case "Gatling" -> Gatling::new;
+            case "GravityGun" -> GravityGun::new;
             default -> null;
         };
     }
