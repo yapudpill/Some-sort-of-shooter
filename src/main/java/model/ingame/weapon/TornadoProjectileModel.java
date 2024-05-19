cb7bb8ff5f43973b76485de37051afc2f159c187
Turbien <clemeblav@gmail.com>
2024-05-19 21:20:41 +0200

    -tornados push enemies

--- src/main/java/model/ingame/weapon/TornadoProjectileModel.java
+++ src/main/java/model/ingame/weapon/TornadoProjectileModel.java
@@ -1,7 +1,10 @@
 package model.ingame.weapon;
 
 import model.ingame.GameModel;
+import model.ingame.entity.CreatureModel;
+import model.ingame.entity.PlayerModel;
 import model.ingame.physics.DamageListener;
+import model.ingame.physics.MovementHandler;
 import util.Coordinates;
 
 public class TornadoProjectileModel extends Projectile{
@@ -14,6 +17,13 @@ public class TornadoProjectileModel extends Projectile{
         super(pos, source, TORNADO_WIDTH, TORNADO_HEIGHT, TORNADO_DAMAGE, gameModel);
         movementHandler.setSpeed(TORNADO_SPEED);
         addCollisionListener(new DamageListener(TORNADO_DAMAGE));
+        addCollisionListener(e-> {
+            e.getInvolvedEntitiesList().forEach(ent -> {
+                if(!(ent instanceof PlayerModel) && ent instanceof CreatureModel c) {
+                    MovementHandler movementHandler = c.getMovementHandler();
+                    movementHandler.setDirectionVector(this.movementHandler.getDirectionVector().add(movementHandler.getDirectionVector()));
+                }
+            });});
         addBlockedMovementListener(e -> despawn());
     }
 }
