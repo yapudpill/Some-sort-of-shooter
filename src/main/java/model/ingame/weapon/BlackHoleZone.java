12d174181606af9954c41bf2272546e7d5d5955f
Turbien <clemeblav@gmail.com>
2024-05-19 13:56:08 +0200

    -fix microbug (enemies not stopping at the center of the black hole)

--- src/main/java/model/ingame/weapon/BlackHoleZone.java
+++ src/main/java/model/ingame/weapon/BlackHoleZone.java
@@ -18,7 +18,7 @@ public class BlackHoleZone extends CollisionEntityModel{
                 if(!(ent instanceof PlayerModel) && ent instanceof CreatureModel c) {
                     // pull the creature towards the black hole
                     MovementHandler movementHandler = c.getMovementHandler();
-                    movementHandler.setDirectionVector(movementHandler.getDirectionVector().add(center.add(c.getPos().opposite()).normalize()));
+                    movementHandler.setDirectionVector(center.add(c.getPos().opposite()).normalize());
                 }
             });
 
